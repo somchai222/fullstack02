40f8c2d0837b159476095c02b3f36209b95b93cf
somchai222 <s6706022511031@email.kmutnb.ac.th>
2026-02-05 16:09:46 +0700

    565

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.send('Hello somchai engineer! 111');
 });
 
 app.listen(port, () => {
